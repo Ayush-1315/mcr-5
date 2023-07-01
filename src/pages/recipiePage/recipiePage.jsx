e22dc2c24c95a7958553d7de601aaa6c2f4a5034
Ayush Raj <ayush.raj138@gmail.com>
2023-07-01 22:01:14 +0530

    feat:added page

--- src/pages/recipiePage/recipiePage.jsx
+++ src/pages/recipiePage/recipiePage.jsx
@@ -2,9 +2,19 @@ import { useParams } from "react-router"
 import { useRecipie } from "../../context/dataContext";
 export const Recipie=()=>{
     const {recipieId}=useParams();
-    const {recipies}=useRecipie()
-    const findRecipie=recipies.find(({_id})=>_id===recipieId)
-    console.log(findRecipie)
+    const {initalRecipie}=useRecipie()
+    const findRecipie=initalRecipie.recipies.find(({_id})=>_id===recipieId)
+    
 
-    return <>Recipie Here</>
+    return <div style={{padding:"1rem"}}>
+    <h1>{findRecipie?.recipie}</h1>
+    <div style={{display:"flex"}}>
+    <img src={findRecipie?.recipieImage} alt="image" style={{height:"20rem"}}/>
+    <div>
+        <h2>Cuisine:{findRecipie?.cusineType}</h2>
+        <b>Ingredients:</b>{findRecipie?.ingredients.map((item,index)=><span key={index}>{item},</span>)}
+        <ol>{findRecipie?.instructions.map((instruction,index)=><li key={index}>{instruction}</li>)}</ol>
+    </div>
+    </div>
+    </div>
 }
